33bce33ea8c249b6dec9a0cb88f8aab15d204050
Omer Abdul <yourname@example.com>
2024-01-09 21:52:20 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const pTag = document.createElement('p');
+pTag.style = 'color:red';
+pTag.textContent = ('hey i am red');
+
+//-----------------------------------------
+
+const h3 = document.createElement('h3');
+h3.textContent = ('i am blue h3');
+h3.style.color = ('blue');
+
+//----------------------------------------
+
